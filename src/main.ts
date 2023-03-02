d95e113df3f2f3d8bba114a8966445f7e1a2bd62
Emerson Moreira <eemr3@yahoo.com.br>
2023-03-01 21:07:54 -0300

    chore: add config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
