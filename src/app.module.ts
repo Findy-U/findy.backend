3dcb2becc05e8120b0ddb3303369b0ea1f2b7648
Fernando D'luccas <dluccash@hotmail.com>
2023-03-13 18:25:54 -0300

    :heavy_plus_sign: feat(config): add @nest/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './application/authentication/auth.module';
+// import { AuthModule } from './application/authentication/auth.module';
 import { CandidateUserModule } from './application/candidate-user/candidate-user.module';
 import appConfig from './config/app/app.config';
+import { EmailModule } from './mails/email/email.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
-    AuthModule,
-    CandidateUserModule
+    // AuthModule,
+    // CandidateUserModule,
+    EmailModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
