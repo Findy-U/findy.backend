3b7db69961e36d6a8072522cc48acc3ae0024d32
Emerson Moreira <eemr3@yahoo.com.br>
2023-03-16 14:02:44 -0300

    :wrench: chore: add lib @nestjs/swagger and config swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,20 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Findy API')
+    .setDescription(
+      'API gerenciamento de candidatos voluntários e projetos voluntários.',
+    )
+    .setVersion('0.0.1')
+    .addTag('candidate_users')
+    .addTag('authentication')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('port'));
   console.log(
