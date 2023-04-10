3ffe45bca211cd9d67b0488109346ab07bb41cee
Mario Lucas <100689099+MarioLucas0@users.noreply.github.com>
2023-04-10 16:38:28 -0300

    :chore: fix: Correction

--- src/application/candidate-profile/candidate-profile.module.ts
+++ src/application/candidate-profile/candidate-profile.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CandidateProfileSQLiteRepository } from 'src/common/repositories/candidate-profile/candidate-profile-sqlite-repository';
 import { CandidateProfileController } from './candidate-profile.controller';
 import { CandidateProfileService } from './candidate-profile.service';
 import { CandidateProfile } from './entities/candidate-profile.entity';
-import { CandidateProfileSQLiteRepository } from 'src/common/repositories/candidate-profile/candidate-profile-sqlite-repository';
-
+import { CandidateUserPostgresRepository } from 'src/common/repositories/candidate-profile/candidate-profile-postgres.repository';
 
 @Module({
   controllers: [CandidateProfileController],
