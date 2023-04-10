4267944fe2a485b34747c093da5fbefa94106a64
Mario Lucas <100689099+MarioLucas0@users.noreply.github.com>
2023-04-10 16:39:48 -0300

    :chore: fix: Correction

--- src/application/candidate-profile/candidate-profile.module.ts
+++ src/application/candidate-profile/candidate-profile.module.ts
@@ -3,6 +3,7 @@ import { CandidateProfileController } from './candidate-profile.controller';
 import { CandidateProfileService } from './candidate-profile.service';
 import { CandidateProfile } from './entities/candidate-profile.entity';
 import { CandidateUserPostgresRepository } from 'src/common/repositories/candidate-profile/candidate-profile-postgres.repository';
+import { CandidateProfileRepository } from 'src/application/candidate-profile/repository/candidate-profile.repository';
 
 @Module({
   controllers: [CandidateProfileController],
