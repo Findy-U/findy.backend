c5bb69f7269e0111c4c0d8b701c57be96992a0a7
ioott <vioott@gmail.com>
2023-06-08 15:07:22 -0300

    fix: add "?" at createdAt and  updatedAt

--- src/common/interfaces/candidate-user-details/candidate-user-details.interface.ts
+++ src/common/interfaces/candidate-user-details/candidate-user-details.interface.ts
@@ -4,6 +4,6 @@ export interface CandidateUserDetailsInterface {
   gender: string;
   birthDate: Date;
   residencePlace: string;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
