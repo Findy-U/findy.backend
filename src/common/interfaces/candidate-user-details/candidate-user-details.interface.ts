6a250d7e44a039f9c71e0e7fd097db848d7ab511
ioott <vioott@gmail.com>
2023-06-08 14:00:39 -0300

    feat: create CandidateUserDetails interface

---
+++ src/common/interfaces/candidate-user-details/candidate-user-details.interface.ts
@@ -0,0 +1,9 @@
+export interface CandidateUserDetailsInterface {
+  id?: number;
+  candidateUserId?: number;
+  gender: string;
+  birthDate: Date;
+  residencePlace: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
