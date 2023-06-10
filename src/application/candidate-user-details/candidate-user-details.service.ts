7f3c564def6001c4f42b2e6f5d722fbb29c5c269
ioott <vioott@gmail.com>
2023-06-10 15:35:32 -0300

    fix: changes dates format

--- src/application/candidate-user-details/candidate-user-details.service.ts
+++ src/application/candidate-user-details/candidate-user-details.service.ts
@@ -3,22 +3,43 @@ import { CreateCandidateUserDetailsDto } from './dto/create-candidate-user-detai
 import { UpdateCandidateUserDetailsDto } from './dto/update-candidate-user-details.dto';
 import { CandidateUserDetailsRepository } from './repositories/candidate-user-details.repository';
 import { NotFoundError } from '../../common/exceptions/not-found.error';
+import { CandidateUserDetailsSerialize } from '../../common/serializers/candidate-user-details.serialize';
 
 @Injectable()
 export class CandidateUserDetailsService {
   constructor(
     private readonly candidateRepository: CandidateUserDetailsRepository,
+    private readonly candidateUserDetailsSerialize: CandidateUserDetailsSerialize,
   ) {}
 
   async create(details: CreateCandidateUserDetailsDto) {
-    const addDetails = await this.candidateRepository.create(details);
-    return this.candidateRepository.create(addDetails);
+    const candidateUserId = await this.candidateRepository.findUnique(
+      details.candidateUserId,
+    );
+    if (candidateUserId) {
+      throw new Error('this user already has registered details');
+    }
+    const userDetails = await this.candidateRepository.create({
+      candidateUserId: details.candidateUserId,
+      gender: details.gender,
+      birthDate: details.birthDate,
+      residencePlace: details.residencePlace,
+      createdAt: details.createdAt,
+      updatedAt: details.updatedAt,
+    });
+    return this.candidateUserDetailsSerialize.dbToResponseCreate(userDetails);
+  }
+
+  async findByCandidateUserId(candidateUserId: number) {
+    return await this.candidateRepository.findUnique(candidateUserId);
   }
 
   async findAll() {
     const details = await this.candidateRepository.findAll();
 
-    return details;
+    return details.map((details) =>
+      this.candidateUserDetailsSerialize.dbToResponse(details),
+    );
   }
 
   async findOne(id: number) {
@@ -27,7 +48,7 @@ export class CandidateUserDetailsService {
       throw new NotFoundError('Candidate not found');
     }
 
-    return this.candidateRepository.findById(candidateDetails);
+    return this.candidateUserDetailsSerialize.dbToResponse(candidateDetails);
   }
 
   async update(
--- src/common/serializers/candidate-user-details.serialize.ts
+++ src/common/serializers/candidate-user-details.serialize.ts
@@ -3,29 +3,43 @@ import { CandidateUserDetailsInterface } from '../interfaces/candidate-user-deta
 export class CandidateUserDetailsSerialize {
   requestToDb(details: CandidateUserDetailsInterface) {
     return {
+      detailsId: details.id,
+      candidateUserId: details.candidateUserId,
       gender: details.gender,
-      birthDate: details.birthDate,
+      birthDate: details.birthDate.toLocaleDateString('pt-BR'),
       residencePlace: details.residencePlace,
+      createdAt: details.createdAt.toLocaleDateString('pt-BR'),
+      updatedAt: details.updatedAt.toLocaleDateString('pt-BR'),
     };
   }
 
   dbToResponseCreate(details: any) {
     return {
-      id: details.id,
+      detailsId: details.id,
+      candidateUserId: details.candidateUserId,
       gender: details.gender,
-      birthDate: details.birthDate,
+      birthDate: details.birthDate.toLocaleDateString('pt-BR'),
       residencePlace: details.residencePlace,
     };
   }
 
   dbToResponse(details: any) {
+    let createdAt = '';
+    let updatedAt = '';
+    details.createdAt
+      ? (createdAt = details.createdAt.toLocaleDateString('pt-BR'))
+      : (createdAt = null);
+    details.updatedAt
+      ? (updatedAt = details.updatedAt.toLocaleDateString('pt-BR'))
+      : (updatedAt = null);
     return {
-      id: details.id,
+      detailsId: details.id,
+      candidateUserId: details.candidateUserId,
       gender: details.gender,
-      birthDate: details.birthDate,
+      birthDate: details.birthDate.toLocaleDateString('pt-BR'),
       residencePlace: details.residencePlace,
-      createdAt: details.createdAt,
-      updatedAt: details.updatedAt,
+      createdAt,
+      updatedAt,
     };
   }
 }
