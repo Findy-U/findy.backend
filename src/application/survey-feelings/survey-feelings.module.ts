1dfcf06c3532a5b3b1cd0baf5d57d0219802c8c8
ioott <vioott@gmail.com>
2023-06-15 14:58:28 -0300

    feat: creates "SurveyFeelings" module

--- src/application/survey-feelings/survey-feelings.module.ts
+++ src/application/survey-feelings/survey-feelings.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SurveyFeelingsService } from './survey-feelings.service';
 import { SurveyFeelingsController } from './survey-feelings.controller';
+import { PrismaService } from 'src/config/database/prisma/prisma.service';
+import { SurveyFeelingsPostgresRepository } from 'src/common/repositories/survey-feelings/survey-feelings-postgres.repository';
+import { SurveyFeelingsSqliteRepository } from 'src/common/repositories/survey-feelings/survey-feelings-sqlite.repository';
+import { SurveyFeelingsRepository } from './repositories/survey-feelings.repository';
 
 @Module({
   controllers: [SurveyFeelingsController],
-  providers: [SurveyFeelingsService],
+  providers: [
+    SurveyFeelingsService,
+    PrismaService,
+    {
+      provide: SurveyFeelingsRepository,
+      useClass: SurveyFeelingsSqliteRepository,
+    },
+  ],
 })
 export class SurveyFeelingsModule {}
