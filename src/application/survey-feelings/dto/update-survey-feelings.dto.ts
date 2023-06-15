2f4aba8ac12c8172c12170ce039aec0c6e7092f6
ioott <vioott@gmail.com>
2023-06-15 14:59:53 -0300

    feat: creates the "CreateSurveyFeelingsDto" and deletes the "UpdateSurveyFeelingsDto"

--- src/application/survey-feelings/dto/update-survey-feelings.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSurveyFeelingsDto } from './create-survey-feelings.dto';
-
-export class UpdateSurveyFeelingsDto extends PartialType(
-  CreateSurveyFeelingsDto,
-) {}
