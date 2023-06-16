9bcf28c6369b65edee307a683a52d3881d976e14
ioott <vioott@gmail.com>
2023-06-16 12:52:52 -0300

    fix: adds SurveyFeelingsSerialize at providers

--- src/application/survey-feelings/survey-feelings.module.ts
+++ src/application/survey-feelings/survey-feelings.module.ts
@@ -5,11 +5,13 @@ import { PrismaService } from 'src/config/database/prisma/prisma.service';
 import { SurveyFeelingsPostgresRepository } from 'src/common/repositories/survey-feelings/survey-feelings-postgres.repository';
 import { SurveyFeelingsSqliteRepository } from 'src/common/repositories/survey-feelings/survey-feelings-sqlite.repository';
 import { SurveyFeelingsRepository } from './repositories/survey-feelings.repository';
+import { SurveyFeelingsSerialize } from 'src/common/serializers/survey-feelings.serialize';
 
 @Module({
   controllers: [SurveyFeelingsController],
   providers: [
     SurveyFeelingsService,
+    SurveyFeelingsSerialize,
     PrismaService,
     {
       provide: SurveyFeelingsRepository,
