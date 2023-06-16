a661117e4a490d876aaf3f81b14e745a1e1875d0
ioott <vioott@gmail.com>
2023-06-16 12:53:38 -0300

    feat: creates SurveyFeelingsController

--- src/application/survey-feelings/survey-feelings.controller.ts
+++ src/application/survey-feelings/survey-feelings.controller.ts
@@ -3,43 +3,100 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
+  UseGuards,
+  Req,
+  ConflictException,
+  NotFoundException,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { SurveyFeelingsService } from './survey-feelings.service';
 import { CreateSurveyFeelingsDto } from './dto/create-survey-feelings.dto';
-import { UpdateSurveyFeelingsDto } from './dto/update-survey-feelings.dto';
+import {
+  ApiBearerAuth,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiExcludeEndpoint,
+  ApiNotFoundResponse,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
+import {
+  ApiConflictResponseCreate,
+  ApiCreatedResponseCreate,
+  ApiParamFindById,
+  ApiResponseFindAll,
+  ApiResponseFindById,
+  NotFoundExceptionError,
+  ResponseFind,
+  UnauthorizedExceptionError,
+} from './swagger/swagger.responses';
 
-@Controller('feelings')
+@Controller('survey-feelings')
+@ApiTags('survey-feelings')
 export class SurveyFeelingsController {
   constructor(private readonly surveyFeelingsService: SurveyFeelingsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createSurveyFeelingsDto: CreateSurveyFeelingsDto) {
-    return this.surveyFeelingsService.create(createSurveyFeelingsDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.surveyFeelingsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.surveyFeelingsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateSurveyFeelingsDto: UpdateSurveyFeelingsDto,
+  @ApiCreatedResponse(ApiCreatedResponseCreate)
+  @ApiConflictResponse(ApiConflictResponseCreate)
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized user',
+    type: UnauthorizedExceptionError,
+  })
+  async create(
+    @Body() createSurveyFeelingsDto: CreateSurveyFeelingsDto,
+    @Req() req: Request,
   ) {
-    return this.surveyFeelingsService.update(+id, updateSurveyFeelingsDto);
+    try {
+      const userAnswers = {
+        ...createSurveyFeelingsDto,
+        candidateUserId: req.user.id,
+      };
+      return await this.surveyFeelingsService.create(userAnswers);
+    } catch (error) {
+      console.log(error);
+      throw new ConflictException(error.message);
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.surveyFeelingsService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @ApiExcludeEndpoint()
+  @ApiBearerAuth()
+  @ApiResponse({ ...ApiResponseFindAll, type: [ResponseFind] })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized user',
+    type: UnauthorizedExceptionError,
+  })
+  async findAll() {
+    return await this.surveyFeelingsService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  @ApiExcludeEndpoint()
+  @ApiBearerAuth()
+  @ApiResponse({ ...ApiResponseFindById, type: ResponseFind })
+  @ApiNotFoundResponse({
+    description: 'Erro quando não encontra o registro no BD',
+    type: NotFoundExceptionError,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized user',
+    type: UnauthorizedExceptionError,
+  })
+  @ApiParam(ApiParamFindById)
+  async findById(@Param('id') id: string) {
+    try {
+      return await this.surveyFeelingsService.findById(+id);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
   }
 }
