6d20b324c08d32842627aaa93fc7e30dbbf96193
Emerson Moreira <eemr3@yahoo.com.br>
2023-09-27 16:58:45 -0300

    refactor: database table candidateProfile and skills

---
+++ prisma/data.ts
@@ -0,0 +1,605 @@
+export const linguagens = [
+  {
+    name: 'python',
+    title: 'Python',
+  },
+  {
+    name: 'javascript',
+    title: 'Javascript',
+  },
+  {
+    name: 'java',
+    title: 'Java',
+  },
+  {
+    name: 'c++',
+    title: 'C++',
+  },
+  {
+    name: 'c#',
+    title: 'C#',
+  },
+  {
+    name: 'php',
+    title: 'PHP',
+  },
+  {
+    name: 'ruby',
+    title: 'Ruby',
+  },
+  {
+    name: 'go',
+    title: 'Go',
+  },
+  {
+    name: 'swift',
+    title: 'Swift',
+  },
+  {
+    name: 'kotlin',
+    title: 'Kotlin',
+  },
+  {
+    name: 'typescript',
+    title: 'Typescript',
+  },
+  {
+    name: 'matlab',
+    title: 'Matlab',
+  },
+  {
+    name: 'rust',
+    title: 'Rust',
+  },
+  {
+    name: 'scala',
+    title: 'Scala',
+  },
+  {
+    name: 'haskell',
+    title: 'Haskell',
+  },
+  {
+    name: 'r',
+    title: 'R',
+  },
+  {
+    name: 'lua',
+    title: 'Lua',
+  },
+  {
+    name: 'perl',
+    title: 'Perl',
+  },
+
+  {
+    name: 'groovy',
+    title: 'Groovy',
+  },
+
+  {
+    name: 'objective-c',
+    title: 'Objective-C',
+  },
+];
+
+export const frameworks = [
+  {
+    name: 'react',
+    title: 'React',
+  },
+  {
+    name: 'angular',
+    title: 'Angular',
+  },
+  {
+    name: 'vue',
+    title: 'Vue.js',
+  },
+  {
+    name: 'django',
+    title: 'Django',
+  },
+  {
+    name: 'spring boot',
+    title: 'Spring Boot',
+  },
+  {
+    name: '.net',
+    title: '.NET Core',
+  },
+  {
+    name: 'flutter',
+    title: 'Flutter',
+  },
+  {
+    name: 'express',
+    title: 'Express',
+  },
+  {
+    name: 'ruby on rails',
+    title: 'Ruby on Rails',
+  },
+  {
+    name: 'laravel',
+    title: 'Laravel',
+  },
+  {
+    name: 'symfony',
+    title: 'Symfony',
+  },
+  {
+    name: 'flask',
+    title: 'Flask',
+  },
+  {
+    name: 'asp.net',
+    title: 'ASP.NET',
+  },
+  {
+    name: 'hibernate',
+    title: 'Hibernate',
+  },
+  {
+    name: 'ember',
+    title: 'Ember.js',
+  },
+  {
+    name: 'backbone',
+    title: 'Backbone.js',
+  },
+  {
+    name: 'xamarin',
+    title: 'Xamarin',
+  },
+  {
+    name: 'electron',
+    title: 'Electron',
+  },
+  {
+    name: 'apache',
+    title: 'Apache',
+  },
+  {
+    name: 'struts',
+    title: 'Struts',
+  },
+  {
+    name: 'play framework',
+    title: 'Play Framework',
+  },
+];
+
+export const databases = [
+  {
+    name: 'mysql',
+    title: 'MySQL',
+  },
+  {
+    name: 'postgresql',
+    title: 'PostgreSQL',
+  },
+  {
+    name: 'mongodb',
+    title: 'MongoDB',
+  },
+  {
+    name: 'oracle',
+    title: 'Oracle Database',
+  },
+  {
+    name: 'firebase',
+    title: 'Firebase',
+  },
+  {
+    name: 'sqlite',
+    title: 'SQLite',
+  },
+  {
+    name: 'mariadb',
+    title: 'MariaDB',
+  },
+  {
+    name: 'ms sql server',
+    title: 'MS SQL Server',
+  },
+  {
+    name: 'cassandra',
+    title: 'Cassandra',
+  },
+  {
+    name: 'redis',
+    title: 'Redis',
+  },
+  {
+    name: 'amazon dynamodb',
+    title: 'Amazon DynamoDB',
+  },
+  {
+    name: 'clouchdb',
+    title: 'ClouchDB',
+  },
+  {
+    name: 'neo4j',
+    title: 'Neo4j',
+  },
+  {
+    name: 'google cloud spanner',
+    title: 'Google Cloud Spanner',
+  },
+  {
+    name: 'ibm db2',
+    title: 'IBM DB2',
+  },
+  {
+    name: 'apache hbse',
+    title: 'Apache HBase',
+  },
+  {
+    name: 'apache kafka',
+    title: 'Apache Kafka',
+  },
+  {
+    name: 'apache cassandra',
+    title: 'Apache Cassandra',
+  },
+  {
+    name: 'arangodb',
+    title: 'ArangoDB',
+  },
+  {
+    name: 'influxdb',
+    title: 'InfluxDB',
+  },
+];
+
+export const toolsDesing = [
+  {
+    name: 'adobe photoshop',
+    title: 'Adobe Photoshop',
+  },
+  {
+    name: 'adobe illustrator',
+    title: 'Adobe Illustrator',
+  },
+  {
+    name: 'adobe xd',
+    title: 'Adobe XD',
+  },
+  {
+    name: 'adobe after effects',
+    title: 'Adobe After Effects',
+  },
+  {
+    name: 'figma',
+    title: 'Figma',
+  },
+  {
+    name: 'sketch',
+    title: 'Sketch',
+  },
+  {
+    name: 'invision',
+    title: 'InVision',
+  },
+  {
+    name: 'marvel mpp',
+    title: 'Marvel MPP',
+  },
+  {
+    name: 'axure rp',
+    title: 'Axure RP',
+  },
+  {
+    name: 'zeplin',
+    title: 'Zeplin',
+  },
+  {
+    name: 'balzamiq',
+    title: 'Balzamiq',
+  },
+  {
+    name: 'principle',
+    title: 'Principle',
+  },
+  {
+    name: 'canva',
+    title: 'Canva',
+  },
+  {
+    name: 'proto.io',
+    title: 'Proto.io',
+  },
+  {
+    name: 'skechup',
+    title: 'Skechup',
+  },
+  {
+    name: 'moqups',
+    title: 'Moqups',
+  },
+  {
+    name: 'uxpin',
+    title: 'UXPin',
+  },
+  {
+    name: 'procreate',
+    title: 'Procreate',
+  },
+  {
+    name: 'affinity designer',
+    title: 'Affinity Designer',
+  },
+  {
+    name: 'craft by invision',
+    title: 'Craft by InVision',
+  },
+];
+
+export const cloudServices = [
+  {
+    name: 'microsft azure',
+    title: 'Microsoft Azure',
+  },
+  {
+    name: 'aws',
+    title: 'AWS (Amazon Web Services)',
+  },
+  {
+    name: 'google cloud',
+    title: 'Google Cloud Platform',
+  },
+  {
+    name: 'oracle cloud',
+    title: 'Oracle Cloud Infrastructure',
+  },
+  {
+    name: 'ibm cloud',
+    title: 'IBM Cloud',
+  },
+  {
+    name: 'sap cloud',
+    title: 'SAP Cloud Platform',
+  },
+  {
+    name: 'alibaba cloud',
+    title: 'Alibaba Cloud',
+  },
+  {
+    name: 'tencent cloud',
+    title: 'Tencent Cloud',
+  },
+  {
+    name: 'digital ocean',
+    title: 'Digital Ocean',
+  },
+  {
+    name: 'baidu cloud',
+    title: 'Baidu Cloud',
+  },
+  {
+    name: 'salesforce cloud',
+    title: 'Salesforce Cloud',
+  },
+  {
+    name: 'adobe cloud',
+    title: 'Adobe Experience Cloud',
+  },
+  {
+    name: 'heroku',
+    title: 'Heroku',
+  },
+  {
+    name: 'dropbox',
+    title: 'Dropbox',
+  },
+  {
+    name: 'vmware cloud',
+    title: 'VMware Cloud',
+  },
+  {
+    name: 'box',
+    title: 'Box',
+  },
+  {
+    name: 'rackspace cloud',
+    title: 'Rackspace Cloud',
+  },
+  {
+    name: 'zoho cloud',
+    title: 'Zoho Cloud',
+  },
+  {
+    name: 'cloudflare',
+    title: 'Cloudflare',
+  },
+  {
+    name: 'openstack',
+    title: 'OpenStack',
+  },
+];
+
+export const toolsDevOps = [
+  {
+    name: 'docker',
+    title: 'Docker',
+  },
+  {
+    name: 'circleci',
+    title: 'CircleCI',
+  },
+  {
+    name: 'jenkins',
+    title: 'Jenkins',
+  },
+  {
+    name: 'bamboo',
+    title: 'Bamboo',
+  },
+  {
+    name: 'kubernetes',
+    title: 'Kubernetes',
+  },
+  {
+    name: 'nagios',
+    title: 'Nagios',
+  },
+  {
+    name: 'gitlabci',
+    title: 'GitLabCI/CD',
+  },
+  {
+    name: 'prometheus',
+    title: 'Prometheus',
+  },
+  {
+    name: 'travis ci',
+    title: 'Travis CI',
+  },
+  {
+    name: 'grafana',
+    title: 'Grafana',
+  },
+  {
+    name: 'ansible',
+    title: 'Ansible',
+  },
+  {
+    name: 'splunk',
+    title: 'Splunk',
+  },
+  {
+    name: 'puppet',
+    title: 'Puppet',
+  },
+  {
+    name: 'elk stack',
+    title: 'ELK Stack (Elasticsearch, Logstash, Kibana)',
+  },
+  {
+    name: 'chef',
+    title: 'Chef',
+  },
+  {
+    name: 'data dog',
+    title: 'Data Dog',
+  },
+  {
+    name: 'vagrant',
+    title: 'Vagrant',
+  },
+  {
+    name: 'new relic',
+    title: 'New Relic',
+  },
+  {
+    name: 'terraform',
+    title: 'Terraform',
+  },
+  {
+    name: 'jira software',
+    title: 'Jira Software',
+  },
+];
+
+export const toolsTesting = [
+  {
+    name: 'junit',
+    title: 'JUnit',
+  },
+  {
+    name: 'karma',
+    title: 'Karma',
+  },
+  {
+    name: 'mocha',
+    title: 'Mocha',
+  },
+  {
+    name: 'protractor',
+    title: 'Protractor',
+  },
+  {
+    name: 'jasmine',
+    title: 'Jasmine',
+  },
+  {
+    name: 'appium',
+    title: 'Appium',
+  },
+  {
+    name: 'selenium',
+    title: 'Selenium',
+  },
+  {
+    name: 'cypress',
+    title: 'Cypress',
+  },
+  {
+    name: 'jest',
+    title: 'Jest',
+  },
+  {
+    name: 'soapui',
+    title: 'SoapUI',
+  },
+  {
+    name: 'testng',
+    title: 'TestNG',
+  },
+  {
+    name: 'gatling',
+    title: 'Gatling',
+  },
+  {
+    name: 'cucumber',
+    title: 'Cucumber',
+  },
+  {
+    name: 'robot framework',
+    title: 'Robot Framework',
+  },
+  {
+    name: 'pytest',
+    title: 'Pytest',
+  },
+  {
+    name: 'mockito',
+    title: 'Mockito',
+  },
+  {
+    name: 'nunit',
+    title: 'NUnit',
+  },
+  {
+    name: 'postman',
+    title: 'Postman',
+  },
+  {
+    name: 'phpunit',
+    title: 'PHPUnit',
+  },
+  {
+    name: 'visual studio test',
+    title: 'Visual Studio Test Professional',
+  },
+];
+
+// [
+//   {
+//     "type": "language",
+//     "name": '"avascr"ip',
+//   },
+//   {
+//     "type": "language",
+//     "name": '"ypescr"ip',
+//   },
+//   {
+//     "type": "language",
+//     "name": "react",
+//   },
+//   {
+//     "type": "database",
+//     "name": "mysql",
+//   },
+// ];
